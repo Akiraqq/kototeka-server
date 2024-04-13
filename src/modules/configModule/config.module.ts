513230cf2e71bf68aa6ee19956dad949978bfb85
Roman Borysenko <romanborysenko2702@gmail.com>
2024-04-13 21:29:42 +0300

    Added docker

---
+++ src/modules/configModule/config.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule as configModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    configModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+})
+export class ConfigModule {}
