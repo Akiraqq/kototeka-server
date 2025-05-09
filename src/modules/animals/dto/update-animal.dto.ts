bfae6cd1cde09cd897516f47a80f8219e5d19f9b
Roman Borysenko <romanborysenko2702@gmail.com>
2025-05-10 00:39:17 +0300

    fix: update UpdateAnimalInput validation

--- src/modules/animals/dto/update-animal.dto.ts
+++ src/modules/animals/dto/update-animal.dto.ts
@@ -1,13 +1,18 @@
 import { Field, ID, InputType, OmitType, PartialType } from '@nestjs/graphql';
 import { CreateAnimalInput } from './create-animal.dto';
+import { IsNotEmpty, IsUUID } from 'class-validator';
 
 @InputType()
 export class UpdateAnimalInput extends PartialType(
   OmitType(CreateAnimalInput, ['organizationId'] as const),
 ) {
+  @IsUUID()
+  @IsNotEmpty()
   @Field(() => ID)
   id: string;
 
+  @IsUUID()
+  @IsNotEmpty()
   @Field(() => ID)
   organizationId: string;
 }
